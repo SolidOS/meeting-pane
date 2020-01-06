4580ef97d3fb5f87ddf534ee606f0a835252ff8f
Michiel de Jong <michiel@unhosted.org>
2020-01-06 11:30:24 +0100

    move JS files into src folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'meetingPane.bundle.js'
