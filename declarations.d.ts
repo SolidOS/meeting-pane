23cd5d88d8d0686be79e73318b7f7e087bfde97c
timea-solid <4144203+timea-solid@users.noreply.github.com>
2026-05-26 16:24:12 +0200

    cleaned out code of typing not needed

--- declarations.d.ts
+++ declarations.d.ts
@@ -2,16 +2,3 @@ declare module '*.ttl' {
   const content: string;
   export default content;
 }
-
-export interface PaneDefinition {
-  icon: string
-  name: string
-  audience: unknown[]
-  label: (subject: unknown, context: unknown) => string | null
-  render: (subject: unknown, context: { dom: Document }) => HTMLElement
-  mintClass?: unknown
-  mintNew?: (context: unknown, options: unknown) => Promise<unknown>
-}
-
-declare const meetingPane: PaneDefinition
-export default meetingPane
